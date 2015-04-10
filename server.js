f97ef9b5bf61cefd07f6f7ee8286f71a6703cf00
PinalBhatt <PinalBhatt@gmail.com>
2015-04-10 11:00:20 -0400

    First Stack on MEAN - NodeJS server initialized

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+var app = express();
+
+//View Engine setup
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.get('*', function(req, res){
+    res.render('index');
+});
+
+app.listen(3000);
