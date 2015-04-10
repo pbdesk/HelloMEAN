15cdd2e6edc4bbb79e7dfd38a768980121ea7406
PinalBhatt <PinalBhatt@gmail.com>
2015-04-10 18:16:26 -0400

    index.jade, rendering bootstrap's offcanvas layout
    plus modifying server.js to server static assets

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var path = require('path');
 var favicon = require('serve-favicon');
 
 var app = express();
 
 //favicon
 app.use(favicon(__dirname + './../public/favicon.ico'));
+
+//Static assets
+app.use(express.static(path.join(__dirname, '../public')));
 
 //View Engine setup
 app.set('views', __dirname + '/views');
