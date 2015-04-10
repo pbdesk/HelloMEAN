4dff2a0000c8f87925d2e9481104ba6527fe0b0c
PinalBhatt <PinalBhatt@gmail.com>
2015-04-10 13:27:14 -0400

    gulp-util printing colorful log with gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
 
 gulp.task('hello-gulp', function() {
     console.log('Hello Gulp - from my first MEAN Project HelloMEAN');
 });
 
 gulp.task('vet', function() {
+    log('Analyzing source .JS files with JSCS & JSHint');
     return gulp
         .src([
             './*.js',
@@ -14,6 +16,21 @@ gulp.task('vet', function() {
         ])
         //.pipe(jscs())
         .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose:true}));
-
+        .pipe(jshint.reporter('jshint-stylish', {verbose:true}))
+        .pipe(jshint.reporter('fail'));
 });
+
+///// Supporting Functions
+function log(msg) {
+    if(typeof(msg) === 'object'){
+        for(var item in msg){
+            if(msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    }
+    else {
+        util.log(util.colors.yellow(msg));
+    }
+
+}
